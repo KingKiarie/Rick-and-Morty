d8681c029e98550ced1f3a7db89a086db1d9eda9
KingKiarie <kiarie7mbugua@gmail.com>
2023-06-08 11:30:02 +0300

    allchar.append => container

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const charsection = document.querySelector('.characters')
+const allCharacters = document.querySelector('.allchar')
 
 async function fetchData(){
     const response = await fetch('https://rickandmortyapi.com/api/character')
@@ -21,11 +22,25 @@ async function fetchData(){
         <h3>${character.status}</h3>
         <h3>${character.location.name}</h3>
         <h4>${character.species}</h4>
+        <a class="Link">View Character details</a>
         
         `
-
+        const link= container.querySelector('.link')
+        link.addEventListener('click', async function(){
+            try{
+                const response = await fetch(`https://rickandmortyapi.com/api/character`)
+                const data = await response.json()
+    
+                localStorage.setItem('character',JSON.stringify(character))
+                window.open('character.html', '_blank')
+            }catch(error){
+                console.log(error )
+            }
+           
+        })
+        
         //append the div element to the body
-        charsection.appendChild(container)
+        allCharacters.appendChild(container)
     })
  
  }
